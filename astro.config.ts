72cb0e727c456c0361eae60ed0dcc60483ae59fa
sikandar <the.sikandar.dev@gmail.com>
2025-08-09 18:21:07 +0530

    feat(registry): add password-input component

--- astro.config.ts
+++ astro.config.ts
@@ -1,146 +1,152 @@
 // @ts-check
-import react from "@astrojs/react";
-import starlight from "@astrojs/starlight";
-import tailwindcss from "@tailwindcss/vite";
-import { defineConfig, envField } from "astro/config";
-import dotenv from "dotenv";
-import starlightThemeBlack from "starlight-theme-black";
+import react from "@astrojs/react"
+import starlight from "@astrojs/starlight"
+import tailwindcss from "@tailwindcss/vite"
+import { defineConfig, envField } from "astro/config"
+import dotenv from "dotenv"
+import starlightThemeBlack from "starlight-theme-black"
 
 // Load Environment Variables
-dotenv.config({ path: ".env.local" });
+dotenv.config({ path: ".env.local" })
 
 // https://astro.build/config
 export default defineConfig({
-    env: {
-        schema: {
-            PUBLIC_SITE_URL: envField.string({
-                context: "client",
-                access: "public",
-            }),
-            PUBLIC_SITE_TITLE: envField.string({
-                context: "client",
-                access: "public",
-            }),
-            PUBLIC_GITHUB_URL: envField.string({
-                context: "client",
-                access: "public",
-            }),
-            PUBLIC_YOUTUBE_URL: envField.string({
-                context: "client",
-                access: "public",
-            }),
-            PUBLIC_TWITTER_URL: envField.string({
-                context: "client",
-                access: "public",
-            }),
-            PUBLIC_GITHUB_REPO_NAME: envField.string({
-                context: "client",
-                access: "public",
-            }),
-            PUBLIC_GITHUB_REPO_URL: envField.string({
-                context: "client",
-                access: "public",
-            }),
-            PUBLIC_SITE_SHORT_TITLE: envField.string({
-                context: "client",
-                access: "public",
-            }),
-            PUBLIC_SITE_DESCRIPTION: envField.string({
-                context: "client",
-                access: "public",
-            }),
-        },
+  site: process.env.PUBLIC_SITE_URL!,
+  env: {
+    schema: {
+      PUBLIC_SITE_URL: envField.string({
+        context: "client",
+        access: "public",
+      }),
+      PUBLIC_SITE_TITLE: envField.string({
+        context: "client",
+        access: "public",
+      }),
+      PUBLIC_GITHUB_URL: envField.string({
+        context: "client",
+        access: "public",
+      }),
+      PUBLIC_YOUTUBE_URL: envField.string({
+        context: "client",
+        access: "public",
+      }),
+      PUBLIC_TWITTER_URL: envField.string({
+        context: "client",
+        access: "public",
+      }),
+      PUBLIC_GITHUB_REPO_NAME: envField.string({
+        context: "client",
+        access: "public",
+      }),
+      PUBLIC_GITHUB_REPO_URL: envField.string({
+        context: "client",
+        access: "public",
+      }),
+      PUBLIC_SITE_SHORT_TITLE: envField.string({
+        context: "client",
+        access: "public",
+      }),
+      PUBLIC_SITE_DESCRIPTION: envField.string({
+        context: "client",
+        access: "public",
+      }),
     },
-    integrations: [
-        react(),
-        starlight({
-            title: process.env.PUBLIC_SITE_TITLE!,
-            description: process.env.PUBLIC_SITE_DESCRIPTION!,
-            customCss: ["./src/styles/global.css"],
-            logo: {
-                dark: "./public/favicon-dark.png",
-                light: "./public/favicon.png",
-                replacesTitle: true,
+  },
+  integrations: [
+    react(),
+    starlight({
+      title: process.env.PUBLIC_SITE_TITLE!,
+      description: process.env.PUBLIC_SITE_DESCRIPTION!,
+      customCss: ["./src/styles/global.css"],
+      favicon: "/favicon.ico",
+      logo: {
+        dark: "./public/favicon-dark.png",
+        light: "./public/favicon.png",
+        replacesTitle: true,
+      },
+      editLink: {
+        baseUrl: `${process.env.PUBLIC_GITHUB_REPO_URL}/tree/main`,
+      },
+      social: [
+        {
+          icon: "github",
+          label: "GitHub",
+          href: process.env.PUBLIC_GITHUB_URL!,
+        },
+        {
+          icon: "youtube",
+          label: "YouTube",
+          href: process.env.PUBLIC_YOUTUBE_URL!,
+        },
+        {
+          icon: "x.com",
+          label: "X.com",
+          href: process.env.PUBLIC_TWITTER_URL!,
+        },
+      ],
+      sidebar: [
+        {
+          label: "Getting Started",
+          items: [
+            {
+              label: "Introduction",
+              slug: "getting-started/introduction",
             },
-            editLink: {
-                baseUrl: `${process.env.PUBLIC_GITHUB_REPO_URL}/tree/main`,
+            {
+              label: "Installation",
+              slug: "getting-started/installation",
             },
-            social: [
-                {
-                    icon: "github",
-                    label: "GitHub",
-                    href: process.env.PUBLIC_GITHUB_URL!,
-                },
-                {
-                    icon: "youtube",
-                    label: "YouTube",
-                    href: process.env.PUBLIC_YOUTUBE_URL!,
-                },
-                {
-                    icon: "x.com",
-                    label: "X.com",
-                    href: process.env.PUBLIC_TWITTER_URL!,
-                },
-            ],
-            sidebar: [
-                {
-                    label: "Getting Started",
-                    items: [
-                        {
-                            label: "Introduction",
-                            slug: "getting-started/introduction",
-                        },
-                        {
-                            label: "Installation",
-                            slug: "getting-started/installation",
-                        },
-                    ],
-                },
-                {
-                    label: "Contributing",
-                    items: [
-                        { label: "Introduction", slug: "contributing" },
-                        {
-                            label: "Component Request",
-                            slug: "contributing/component-request",
-                        },
-                        {
-                            label: "Feature Request",
-                            slug: "contributing/feature-request",
-                        },
-                        {
-                            label: "Contributing Code",
-                            slug: "contributing/contributing-code",
-                        },
-                    ],
-                },
-            ],
-            plugins: [
-                starlightThemeBlack({
-                    navLinks: [
-                        {
-                            label: "Docs",
-                            link: "/getting-started/installation",
-                        },
-                        {
-                            label: "Components",
-                            link: "/components",
-                        },
-                        {
-                            label: "Contributing",
-                            link: "/contributing",
-                        },
-                    ],
-                    footerText:
-                        "Built by [Sikandar Dev](https://www.github.com/sikandardeveloper) for use with [Shadcn](https://ui.shadcn.com).",
-                }),
-            ],
+          ],
+        },
+        {
+          label: "Components",
+          autogenerate: { directory: "components" },
+        },
+        {
+          label: "Contributing",
+          items: [
+            { label: "Introduction", slug: "contributing" },
+            {
+              label: "Component Request",
+              slug: "contributing/component-request",
+            },
+            {
+              label: "Feature Request",
+              slug: "contributing/feature-request",
+            },
+            {
+              label: "Contributing Code",
+              slug: "contributing/contributing-code",
+            },
+          ],
+        },
+      ],
+      plugins: [
+        starlightThemeBlack({
+          navLinks: [
+            {
+              label: "Docs",
+              link: "/getting-started/installation",
+            },
+            {
+              label: "Components",
+              link: "/components",
+            },
+            {
+              label: "Contributing",
+              link: "/contributing",
+            },
+          ],
+          footerText:
+            "Built by [Sikandar Dev](https://www.github.com/sikandardeveloper) for use with [Shadcn](https://ui.shadcn.com).",
         }),
-    ],
-    vite: {
-        plugins: [tailwindcss()],
-        ssr: {
-            noExternal: ["zod"],
-        },
+      ],
+    }),
+  ],
+  vite: {
+    plugins: [tailwindcss()],
+    ssr: {
+      noExternal: ["zod"],
     },
-});
+  },
+})
